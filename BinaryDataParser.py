f92527d5619ebf1bc8a185d0e34b6514f16cb526
George Thomas <georgetamayothomas@gmail.com>
2015-01-11 23:49:28 +0000

    Parser for processing data from the accelerometer

---
+++ BinaryDataParser.py
@@ -0,0 +1,29 @@
+__author__ = 'George'
+
+import struct
+from datetime import datetime
+
+class DataRow:
+    def __init__(self, time, x, y, z):
+        self.time = time
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):
+        return str(self.time) + " " + str(self.x) + " " + str(self.y) + " " + str(self.z)
+
+    @classmethod
+    def from_row(self, row):
+        timestamp, x, y, z = struct.unpack('>qfff', row)
+        timestamp = datetime.fromtimestamp(timestamp)
+        return DataRow(timestamp, x, y, z)
+
+def print_rows_from_file():
+    with open("assets/data/20150111133723.dat", "rb") as f:
+        bytes = f.read(20)
+        while bytes != b"":
+            print(DataRow.from_row(bytes))
+            bytes = f.read(20)
+
+print_rows_from_file()
