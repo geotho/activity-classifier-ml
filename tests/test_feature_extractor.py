6ec9eaeea26df564c1f375279f6e3b5ce96d6903
George Thomas <georgetamayothomas@gmail.com>
2015-01-17 19:18:25 +0000

    Optimistic test class

---
+++ tests/test_feature_extractor.py
@@ -0,0 +1,11 @@
+__author__ = 'George'
+
+import unittest
+
+class TestFeatureExtractor(unittest.TestCase):
+
+    def testTrue(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
