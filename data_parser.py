8c73c3ab77562dd1e4c01eeac5da21396ac77027
George Thomas <georgetamayothomas@gmail.com>
2015-01-17 15:12:31 +0000

    Cosmetic changes

--- data_parser.py
+++ data_parser.py
@@ -24,11 +24,13 @@ def generate_additional_columns(old_array):
 def simple_plot(array):
     # plot(array['timestamp'], array['x'], 'r', array['timestamp'], array['y'], 'g', array['timestamp'], array['z'], 'b')
     plot(array['timestamp'], array['magnitude'])
-    xlabel('time (s)')
+    xlabel('time (ns)')
     ylabel('acceleration (m/s2)')
     title('Plot of data')
     grid(True)
     show()
 
 data = generate_additional_columns(make_array_from_file("assets/data/accelData20150104.dat"))
+print(data)
+
 simple_plot(data)
