625c0ab744a826134a1142770ac178398337a857
George Thomas <georgetamayothomas@gmail.com>
2015-01-17 19:18:44 +0000

    Extract a few features an array

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,4 +1,17 @@
 __author__ = 'George'
 from pylab import *
 
-feature_row_datatype = np.dtype([('x_bar', '>f4'), ('y_bar', '>f4'), ('z_bar', '>f4'), ('magnitude_bar', '>f4')])
+feature_row_datatype = np.dtype([('x_std', '>f8'),
+                                 ('y_std', '>f8'),
+                                 ('z_std', '>f8'),
+                                 ('magnitude_bar', '>f8'),
+                                 ('magnitude_std', '>f8')])
+
+
+def extract_features(a: np.ndarray):
+    features = np.empty((1, len(feature_row_datatype)), dtype=feature_row_datatype)
+    features['x_std'] = np.std(a['x'])
+    features['y_std'] = np.std(a['y'])
+    features['z_std'] = np.std(a['z'])
+    features['magnitude_bar'] = np.mean(a['magnitude'])
+    features['magnitude_std'] = np.std(a['magnitude'])
