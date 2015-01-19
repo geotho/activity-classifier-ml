83df68fc14d3f8297c62cd49ca3d4135f0bda58a
George Thomas <georgetamayothomas@gmail.com>
2015-01-19 16:46:43 +0000

    Store timestamp in seconds

--- data_parser.py
+++ data_parser.py
@@ -3,7 +3,7 @@ from pylab import *
 __author__ = 'George'
 
 file_datatype = np.dtype([('timestamp', '>i8'), ('x', '>f4'), ('y', '>f4'), ('z', '>f4')])
-processed_datatype = np.dtype(file_datatype.descr + [('magnitude', '>f4')])
+processed_datatype = np.dtype([('timestamp', '>f12'), ('x', '>f4'), ('y', '>f4'), ('z', '>f4'), ('magnitude', '>f4')])
 
 def make_array_from_file(filename):
     data = np.fromfile(open(filename, "rb"), file_datatype)
@@ -13,7 +13,7 @@ def make_array_from_file(filename):
 def generate_additional_columns(old_array):
     new_array = np.empty(old_array.shape, dtype=processed_datatype)
     # TODO: find a better way of copying array into another
-    new_array['timestamp'] = (old_array['timestamp'] - old_array['timestamp'][0])
+    new_array['timestamp'] = (old_array['timestamp'] - old_array['timestamp'][0])/1000000000
     for label in ['x', 'y', 'z']:
         new_array[label] = old_array[label]
     new_array['magnitude'] = np.sqrt(old_array['x'] ** 2 + old_array['y'] ** 2 + old_array['z'] ** 2)
@@ -24,6 +24,7 @@ def simple_plot(array):
     # plot(array['timestamp'], array['x'], 'r', array['timestamp'], array['y'], 'g', array['timestamp'], array['z'], 'b')
     plot(array['timestamp'], array['magnitude'])
     xlabel('time (ns)')
+    xlabel('time (s)')
     ylabel('acceleration (m/s2)')
     title('Plot of data')
     grid(True)
