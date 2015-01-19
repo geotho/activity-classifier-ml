e115b35c38920c24ec3c90bb1135e4d5fa231d53
George Thomas <georgetamayothomas@gmail.com>
2015-01-19 16:47:38 +0000

    Fixes to the feature extractor

--- feature_extractor.py
+++ feature_extractor.py
@@ -9,9 +9,10 @@ feature_row_datatype = np.dtype([('x_std', '>f8'),
 
 
 def extract_features(a: np.ndarray):
-    features = np.empty((1, len(feature_row_datatype)), dtype=feature_row_datatype)
+    features = np.empty((1, 1), dtype=feature_row_datatype)
     features['x_std'] = np.std(a['x'])
     features['y_std'] = np.std(a['y'])
     features['z_std'] = np.std(a['z'])
     features['magnitude_bar'] = np.mean(a['magnitude'])
     features['magnitude_std'] = np.std(a['magnitude'])
+    return features
