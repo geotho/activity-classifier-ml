39d288b4382569bd8563e115a764207b7ef23aa5
George Thomas <georgetamayothomas@gmail.com>
2015-04-24 21:23:03 +0100

    Use SQLite db for data

--- data_parser.py
+++ data_parser.py
@@ -5,8 +5,9 @@ import pandas as pd
 import scipy as sp
 import os
 import math
+import sqlite3
 from sklearn.metrics.metrics import f1_score
-from sklearn import preprocessing
+from sklearn import preprocessing, metrics
 from sklearn import tree
 from sklearn import dummy
 from sklearn import naive_bayes
@@ -14,11 +15,14 @@ from sklearn import ensemble
 from pandas.tools.plotting import autocorrelation_plot
 from scipy import signal
 from sklearn.cross_validation import StratifiedShuffleSplit
+import sys
 
 __author__ = 'George'
 
 file_datatype = np.dtype([('timestamp', '>i8'), ('x', '>f4'), ('y', '>f4'), ('z', '>f4')])
 processed_datatype = np.dtype([('timestamp', '<f12'), ('x', '<f4'), ('y', '<f4'), ('z', '<f4'), ('magnitude', '<f4')])
+latex_directory = '/Users/George/Documents/dissertation/Part-II-Dissertation/'
+label_encoder = None
 
 
 def make_array_from_file(filename):
@@ -95,10 +99,9 @@ def sampling_frequency(df):
     return n / (df.index.max()-df.index.min())
 
 
-def low_pass_filter(df):
+def low_pass_filter(df, cut_off=5):
     N = 4
     fs = sampling_frequency(df)
-    cut_off = 5
     b, a = signal.butter(N, cut_off/(fs/2.0), btype='low', analog=False, output='ba')
     data_f = pd.DataFrame(signal.filtfilt(b, a, df, axis=0))
     data_f.index = df.index
@@ -132,6 +135,13 @@ def welch(df):
     return sp.signal.welch(df, fs, nperseg=1024)
 
 
+def window(df):
+    rows, cols = df.shape
+    windowing_function = np.hanning
+    window = pd.concat([pd.Series(windowing_function(len(df)))] * cols, axis=1)
+    return pd.DataFrame(window.values * df.values, columns=df.columns, index=df.index)
+
+
 def simple_plot(array1, array2=None, filename="Default.pdf"):
     # plot(array['timestamp'], array['x'], 'r', array['timestamp'], array['y'], 'g', array['timestamp'], array['z'], 'b')
     if array2 is None:
@@ -148,7 +158,13 @@ def simple_plot(array1, array2=None, filename="Default.pdf"):
 
 def data_set_from_files():
     data_set = None
-    filenames = {}
+    # filenames = {}
+    conn = sqlite3.connect('recording_database.db')
+    c = conn.cursor()
+
+    c.execute('''CREATE TABLE data_sets
+        (timestamp text, device text, user text, activity text, filename text)''')
+    conn.commit()
 
     global data_sets
     data_sets = {}
@@ -157,11 +173,15 @@ def data_set_from_files():
     for i in os.listdir(data_directory):
         if i.endswith(".dat"):
             timestamp, device, user, activity = i[:-4].lower().split('-')
-            if device == 'phone':
-                filenames[(timestamp, device, user, activity)] = data_directory + i
-                print(i)
+            filename = data_directory + i
+            c.execute("INSERT INTO data_sets VALUES (?, ?, ?, ?, ?)",
+                      (timestamp, device, user, activity, filename))
+            conn.commit()
 
-    for (timestamp, device, user, activity), phone_filename in filenames.items():
+    # for (timestamp, device, user, activity), phone_filename in filenames.items():
+    query = "SELECT * FROM data_sets WHERE device='phone'"
+    c.execute(query)
+    for (timestamp, device, user, activity, phone_filename) in c.fetchall():
         phone_data, wear_data = make_dataframes_from_filename(phone_filename)
 
         data_sets[(timestamp, 'phone', user, activity)] = phone_data
@@ -239,11 +259,15 @@ def data_set_from_files():
     return data_set, labels, binary_labels, phone_features.columns, wear_features.columns, le
 
 
-def generate_f1(data_set, labels, phone_columns, wear_columns, classifiers, label_encoder):
+def generate_f1(data_set, labels, phone_columns, wear_columns, classifiers):
     f1 = {}
-    sss = StratifiedShuffleSplit(labels, 10, test_size=0.3)
+    error = {}
+    confusion = {}
+    sss = StratifiedShuffleSplit(labels, 10, test_size=0.5)
     for c in classifiers:
         f1[c.__name__] = {}
+        error[c.__name__] = {}
+        confusion[c.__name__] = {}
 
     for train_indexes, test_indexes in sss:
         train = data_set.iloc[train_indexes]
@@ -262,46 +286,87 @@ def generate_f1(data_set, labels, phone_columns, wear_columns, classifiers, labe
 
         for c in classifiers:
             scores = f1[c.__name__]
+            confusion_for_classifier = confusion[c.__name__]
 
-            clf = c()
-            clf.fit(train, train_labels)
-            results = pd.DataFrame(clf.predict(test))
-            if 'both' in scores:
-                scores['both'] = np.vstack((scores['both'], f1_score(test_labels, results, average=None)))
-            else:
-                scores['both'] = f1_score(test_labels, results, average=None)
+            for name, tr, te in [('both', train, test), ('phone', train_phone, test_phone), ('wear', train_wear, test_wear)]:
+                clf = c()
+                clf.fit(tr, train_labels)
+                results = pd.DataFrame(clf.predict(te))
+                if name in scores:
+                    if c.__name__ == 'DecisionTreeClassifier' and name == 'both':
+                        tree.export_graphviz(clf,
+                                             out_file=latex_directory + 'figs/tree.dot',
+                                             feature_names=phone_columns | wear_columns)
+                    scores[name] = np.vstack((scores[name], f1_score(test_labels, results, average=None)))
+                    confusion_for_classifier[name] += \
+                        metrics.confusion_matrix(test_labels.values, results.values, labels=label_encoder.classes_)
+                else:
+                    scores[name] = f1_score(test_labels, results, average=None)
+                    confusion_for_classifier[name] = \
+                        metrics.confusion_matrix(test_labels.values, results.values, labels=label_encoder.classes_)
 
             # print(dict(zip(list(train.columns), list(clf.feature_importances_))))
 
-            clf = c()
-            clf.fit(train_phone, train_labels)
-            results = pd.DataFrame(clf.predict(test_phone))
-            if 'phone' in scores:
-                scores['phone'] = np.vstack((scores['phone'], f1_score(test_labels, results, average=None)))
-            else:
-                scores['phone'] = f1_score(test_labels, results, average=None)
-
-            clf = c()
-            clf.fit(train_wear, train_labels)
-            results = pd.DataFrame(clf.predict(test_wear))
-            if 'wear' in scores:
-                scores['wear'] = np.vstack((scores['wear'], f1_score(test_labels, results, average=None)))
-            else:
-                scores['wear'] = f1_score(test_labels, results, average=None)
-
+            # clf = c()
+            # clf.fit(train_phone, train_labels)
+            # results = pd.DataFrame(clf.predict(test_phone))
+            # if 'phone' in scores:
+            #     scores['phone'] = np.vstack((scores['phone'], f1_score(test_labels, results, average=None)))
+            #     confusion_for_classifier['both'] += \
+            #         metrics.confusion_matrix(test_labels, results, labels=label_encoder.classes_)
+            # else:
+            #     scores['phone'] = f1_score(test_labels, results, average=None)
+            #
+            # clf = c()
+            # clf.fit(train_wear, train_labels)
+            # results = pd.DataFrame(clf.predict(test_wear))
+            # if 'wear' in scores:
+            #     scores['wear'] = np.vstack((scores['wear'], f1_score(test_labels, results, average=None)))
+            # else:
+            #     scores['wear'] = f1_score(test_labels, results, average=None)
 
     for k, v in f1.items():
         for k1, v1 in f1[k].items():
             f1[k][k1] = np.mean(v1, axis=0)
+            error[k][k1] = np.std(v1, axis=0)/np.sqrt(10)
         f1[k] = pd.DataFrame.from_dict(f1[k])
+        error[k] = pd.DataFrame.from_dict(error[k])
         f1[k].index = label_encoder.inverse_transform(f1[k].index)
-    return f1
+        error[k].index = label_encoder.inverse_transform(error[k].index)
+
+    return f1, confusion
+
+
+def output_data_set_metadata(labels):
+    df = pd.DataFrame()
+    vc = labels.value_counts()
+    df['Counts'] = vc
+    df['Proportion'] = labels.value_counts(normalize=True)
+    df['Time recorded (minutes)'] = (df['Counts'] / 6).apply(round).apply(int)
+    d = {'Counts': df['Counts'].sum(),
+         'Proportion': 1,
+         'Time recorded (minutes)': df['Time recorded (minutes)'].sum()}
+    df.set_index(label_encoder.inverse_transform(df.index.values), inplace=True)
+    total = pd.DataFrame(data=d, index=['Total'])
+    df = df.append(total)
+    df.insert(0, 'Activity', df.index)
+    df['Activity'] = df['Activity'].apply(lambda x: x[:1].upper() + x[1:])
+    f = open(latex_directory + 'data/TableDataSetMetadata.tex', 'w')
+    df.to_latex(buf=f, float_format=lambda x: '{:.2%}'.format(x), index=False)
 
 
 def main():
     plt.ioff()
 
+    try:
+        os.remove('recording_database.db')
+    except OSError:
+        pass
+
+    global label_encoder
     data_set, labels, binary_labels, phone_columns, wear_columns, label_encoder = data_set_from_files()
+
+    output_data_set_metadata(labels)
 
     classifiers = [tree.DecisionTreeClassifier,
                    dummy.DummyClassifier,
@@ -309,11 +374,15 @@ def main():
                    naive_bayes.GaussianNB,
                    ]
 
-    f1 = generate_f1(data_set, labels, phone_columns, wear_columns, classifiers, label_encoder)
+    global f1
+    global confusion_matrices
+    f1, confusion_matrices = generate_f1(data_set, labels, phone_columns, wear_columns, classifiers)
 
     for k, v in f1.items():
         print(k)
         print(v)
+        f = open(latex_directory + '/data/' + 'Table' + k + '.tex', 'w')
+        v.to_latex(buf=f, float_format=lambda x: '%0.3f' % x)
 
     print("Improvement of RandomForestClassifier over DummyClassifier")
     print(f1['RandomForestClassifier'] - f1['DummyClassifier'])
