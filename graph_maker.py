c5769df9ef4a1ed9d193e8e68e995953d5521983
George Thomas <georgetamayothomas@gmail.com>
2015-04-24 21:22:37 +0100

    Graph maker for each activitiy

--- graph_maker.py
+++ graph_maker.py
@@ -1,9 +1,14 @@
-from data_parser import make_dataframes_from_filename
+import sqlite3
+from data_parser import make_dataframes_from_filename, power_spectrum, fourier_transform, low_pass_filter, window
 import matplotlib.pyplot as plt
 import pandas as pd
 import scipy as sp
 import numpy as np
+from matplotlib import rc
 import math
+
+# rc('font',**{'family':'serif','serif':['Palatino']})
+# rc('text', usetex=True)
 
 figs_path = '/Users/George/Documents/dissertation/Part-II-Dissertation/figs/'
 
@@ -146,8 +151,120 @@ def make_noise_probability_plot(data):
 
     plt.savefig(figs_path + 'noise_prob_plot.pdf')
 
+
+def make_filter_frequency_response_plot():
+    plot_common()
+    # n = 5
+
+    fs = 50
+    cut_off = 5
+    for i in range(1, 6):
+        b, a = sp.signal.butter(i, cut_off/(fs/2.0), btype='low', analog=False, output='ba')
+        w, h = sp.signal.freqz(b, a)
+
+        plt.plot(0.5*fs*w/np.pi, np.abs(h), color=tableau20[i-1])
+
+    plt.title('Butterworth filter frequency response', fontsize=16)
+    plt.xlabel('Frequency (Hz)', fontsize=16)
+    plt.ylabel('Relative amplitude', fontsize=16)
+    plt.margins(0, 0.1)
+
+    # plt.plot(cut_off, 0.5*np.sqrt(2), 'ko')
+    # plt.grid(which='both', axis='both')
+    plt.legend(list(map(lambda x: "order = " + str(x), range(1,6))))
+
+    plt.axvline(cut_off, ls='--')  # cutoff frequency
+    plt.text(5.2, 0.9, 'Cut off frequency = 5 Hz', ha='left')
+
+    plt.savefig(figs_path + 'butterworth_filters.pdf')
+    # plt.show()
+
+
+def make_2by2_of_all_activities():
+    conn = sqlite3.connect('recording_database.db')
+    c = conn.cursor()
+    activities = c.execute("SELECT DISTINCT activity FROM data_sets").fetchall()
+    activities = list(map(lambda x: x[0], activities))
+
+    time = 100
+
+    for activity in activities:
+        phone_filename = c.execute('''SELECT filename FROM data_sets
+                WHERE activity = ?
+                AND device = 'phone'
+                ORDER BY timestamp DESC''', (activity,)).fetchone()
+        print(phone_filename[0])
+
+        phone_data, wear_data = make_dataframes_from_filename(phone_filename[0])
+
+        snip = lambda x: x[(x.index > time) & (x.index < time+10)]
+
+        phone_data = low_pass_filter(snip(phone_data))
+        wear_data = low_pass_filter(snip(wear_data))
+
+        phone_data.set_index(phone_data.index - time, inplace=True)
+        wear_data.set_index(wear_data.index - time, inplace=True)
+
+        # phone_data = window(phone_data)
+        # wear_data = window(wear_data)
+
+        make_2by2_graph_of_activity(phone_data, wear_data, activity)
+
+
+def make_2by2_graph_of_activity(phone_data, wear_data, activity=""):
+    plot_common()
+    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+
+    for ax in [ax1, ax2, ax3, ax4]:
+        ax.ticklabel_format(axis='y', style='sci', scilimits=(-2, 2))
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.get_xaxis().tick_bottom()
+        ax.get_yaxis().tick_left()
+
+    ax1.plot(phone_data.index, phone_data.x, color=tableau20[0])
+    ax1.plot(phone_data.index, phone_data.y, color=tableau20[1])
+    ax1.plot(phone_data.index, phone_data.z, color=tableau20[2])
+    plt.suptitle("Activity = " + activity, fontsize=16)
+
+    ax1.set_title('Phone magnitude', fontsize=12)
+    ax1.set_xlabel('Time (s)', fontsize=12)
+    ax1.set_ylabel("Acceleration (" + r'$\mathrm{ms}^{-2}$' + ")", fontsize=12)
+    ax1.plot(phone_data.index, phone_data.magnitude, color='k')
+
+    phone_power_spec = power_spectrum(fourier_transform(phone_data.magnitude))
+    phone_power_spec = phone_power_spec[phone_power_spec.index < 10]
+    ax2.set_title('Fourier transform of\nphone magnitude', fontsize=12)
+    ax2.set_xlabel('Frequency (Hz)', fontsize=12)
+    ax2.set_ylabel('Power spectral density', fontsize=12)
+    ax2.set_xlim([0, 6])
+    ax2.plot(phone_power_spec.index, phone_power_spec[0], color=tableau20[0])
+
+    ax3.plot(wear_data.index, wear_data.x, color=tableau20[0])
+    ax3.plot(wear_data.index, wear_data.y, color=tableau20[1])
+    ax3.plot(wear_data.index, wear_data.z, color=tableau20[2])
+
+    ax3.set_title('Watch magnitude', fontsize=12)
+    ax3.set_xlabel('Time (s)', fontsize=12)
+    ax3.set_ylabel("Acceleration (" + r'$\mathrm{ms}^{-2}$' + ")", fontsize=12)
+    ax3.plot(wear_data.index, wear_data.magnitude, color='k')
+
+    wear_power_spec = power_spectrum(fourier_transform(wear_data.magnitude))
+    wear_power_spec = wear_power_spec[wear_power_spec.index < 10]
+    ax4.set_title('Fourier transform of\nwatch magnitude', fontsize=12)
+    ax4.set_xlabel('Frequency (Hz)', fontsize=12)
+    ax4.set_ylabel('Power spectral density', fontsize=12)
+    ax4.set_xlim([0, 6])
+    ax4.plot(wear_power_spec.index, wear_power_spec[0], color=tableau20[0])
+
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.85)
+    plt.savefig(figs_path + '2by2' + activity + '.pdf')
+
 plt.ioff()
 # make_noise_histogram(phone_data_phone_flat_1hr)
 # make_x_y_z_flat_plot(phone_data_phone_flat_1hr)
 # make_timestamp_histogram(phone_data_phone_flat_1hr)
-make_noise_probability_plot(phone_data_phone_flat_1hr)
+# make_noise_probability_plot(phone_data_phone_flat_1hr)
+# make_filter_frequency_response_plot()
+make_2by2_of_all_activities()
